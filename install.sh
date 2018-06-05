2e472a17a5523ca73855bf97d6e731f64becf07d
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2018-06-05 00:20:57 -0300

    Remove ms stuff

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-luarocks --tree=luarocks install --server=http://luarocks.org/dev sitegen
-
-luarocks path --bin > activate.sh
-sed -i "s:$HOME/.luarocks:$(pwd)/luarocks:g" activate.sh
-
--- site/blog/index.md
+++
@@ -1,5 +0,0 @@
-# This is blog index
-
-
-Wow such index
-
